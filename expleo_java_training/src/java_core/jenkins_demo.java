2ba84026128bc4c730bfa076d3f04a406b863732
krishnaprasath <prasathkrishna002@gmail.com>
2026-03-09 15:20:43 +0530

    added

---
+++ expleo_java_training/src/java_core/jenkins_demo.java
@@ -0,0 +1,14 @@
+package java_core;
+import java.util.Scanner;
+public class jenkins_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    Scanner sc = new Scanner(System.in);
+    System.out.print("enter: ");
+    String s = sc.nextLine();
+    System.out.println("your name is: "+s);
+    sc.close();
+	}
+
+}
